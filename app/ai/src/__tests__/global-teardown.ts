50f520c9fba410973a71bbcd4f7cce4c7c2dd032
kryntelxf <woseltehateha@gmail.com>
2026-09-14 10:09:21 +0700

    Add Jest global teardown function

---
+++ app/ai/src/__tests__/global-teardown.ts
@@ -0,0 +1,9 @@
+/**
+ * VYENFITA Jest Global Teardown
+ * 
+ * @version 1.0.0
+ */
+
+export default async function globalTeardown(): Promise<void> {
+  console.log('[Jest] Global teardown complete');
+}
